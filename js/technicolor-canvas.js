e04e6a21e8b5c7c9136910bfdb6cc3e11aeeb52a
Gary Johnson <jazzbassjohnson@gmail.com>
2013-12-25 13:22:30 -0800

    more funtionality

--- js/technicolor-canvas.js
+++ js/technicolor-canvas.js
@@ -8,6 +8,28 @@ $(document).ready(function(){
    		$('#color_3').css('background-color', randomColor());
    		$('#color_4').css('background-color', randomColor());
    });
+   function freeBrush(){
+   		$(".box").on("mouseenter", function () {
+			$(this).css('background-color', paint);
+			$(this).css('box-shadow', "0 0 8px white");
+			$(this).css('z-index', "1000");
+		});
+
+		$(".box").on("mouseleave", function () {
+			$(this).css('box-shadow', "none");
+			$(this).css('z-index', "1");
+		});
+   }
+
+   var paint = '#777';
+
+   $('.color').on('click', function(){
+   		paint = $(this).css('background-color');
+   		$('.box').on('click', function(){
+   		$(this).css('background-color', paint);
+   		freeBrush();	
+   });
+   });
 
 
 
