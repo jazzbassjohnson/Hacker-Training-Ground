bd12600f2c4cc5d349c5c3b840cd25ce3b0fc1dc
Gary Johnson <jazzbassjohnson@gmail.com>
2013-12-25 10:38:47 -0800

    Added color functionality, paint buckets, and canvas grid

--- js/technicolor-canvas.js
+++ js/technicolor-canvas.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
-   $('#randomize').on('click'), function(){
-   	
-   } 
+	function randomColor(){
+		return "#" + Math.random().toString(16).slice(2, 8)
+	}
+   $('#randomize').on('click', function(){
+   		$('#color_1').css('background-color', randomColor());
+   		$('#color_2').css('background-color', randomColor());
+   		$('#color_3').css('background-color', randomColor());
+   		$('#color_4').css('background-color', randomColor());
+   });
+
+
+
+
+
+
+
+
+
+
+
+
 });
